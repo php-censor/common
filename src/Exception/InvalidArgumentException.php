a8ba554ee7b40b6820e7d338b6c1e90a12c3af12
Dmitry Khomutov <poisoncorpsee@gmail.com>
2021-05-18 22:43:39 +0700

    Added common exceptions.

---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace PHPCensor\Common\Exception;
+
+/**
+ * @package    PHP Censor
+ * @subpackage Common Library
+ *
+ * @author Dmitry Khomutov <poisoncorpsee@gmail.com>
+ */
+class InvalidArgumentException extends LogicException
+{
+}
